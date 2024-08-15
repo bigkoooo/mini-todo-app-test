e733a17933b04cc5be75a164653ded1a1f324aac
bigkoooo <kimminseok8910@gmail.com>
2024-08-15 15:01:48 +0900

    onkeydown수정정

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -3,16 +3,21 @@ import React, { useState } from 'react';
 export default function AddTodo({ addItem }) {
   const [todoItem, setTodoItem] = useState({
     title: '',
-  }); // 사용자 입력을 저장할 객체 (id, title, done에 대한 정보를 저장해야하므로 객체)
+  });
 
   const onButtonClick = () => {
-    addItem(todoItem); // add 함수 사용.
+    addItem(todoItem);
     setTodoItem({
-      title: '', // 상태 초기화
+      title: '',
     });
   };
 
-  // 과제1) add enter키로 실행.
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      onButtonClick();
+    }
+  };
+
   return (
     <div className="AddTodo">
       <input
@@ -20,6 +25,7 @@ export default function AddTodo({ addItem }) {
         placeholder="Add your new Todo"
         value={todoItem.title}
         onChange={(e) => setTodoItem({ title: e.target.value })}
+        onKeyDown={handleKeyDown} // onKeyPress 대신 onKeyDown 사용
       />
       <button onClick={onButtonClick}>ADD</button>
     </div>
